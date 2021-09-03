d8642ff7d52e52949e997767e3ea702577bbe496
AlexNerby <alexnerbygit@gmail.com>
2021-09-03 11:07:59 +0300

    BaseModelFactory

--- app/src/main/java/ru/inc/myalarm/di/factory/ViewModelFactory.kt
+++ app/src/main/java/ru/inc/myalarm/di/factory/ViewModelFactory.kt
@@ -1,26 +1,10 @@
 package ru.inc.myalarm.di.factory
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import ru.inc.myalarm.model.repositories.AlarmServiceChange
-import ru.inc.myalarm.view_model.main.MainAlarmRepository
-import ru.inc.myalarm.view_model.main.MainViewModel
-import java.lang.IllegalArgumentException
 import javax.inject.Inject
-import javax.inject.Named
 import javax.inject.Provider
 import javax.inject.Singleton
 
 @Singleton
-class ViewModelFactory @Inject constructor(
-    private val viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>,
-): ViewModelProvider.Factory {
-//some changes
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        val provider = viewModels[modelClass]
-            ?: throw IllegalArgumentException("Unknown model class $modelClass")
-
-        return provider.get() as T
-    }
-}
+class ViewModelFactory @Inject constructor(viewModels: MutableMap<Class<out ViewModel>, Provider<ViewModel>>) :
+    BaseModelFactory(viewModels)
--- app/src/main/java/ru/inc/myalarm/model/repositories/CreateAlarmRepositoryImpl.kt
+++ app/src/main/java/ru/inc/myalarm/model/repositories/CreateAlarmRepositoryImpl.kt
@@ -2,15 +2,13 @@ package ru.inc.myalarm.model.repositories
 
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import ru.inc.myalarm.di.scope.DetailsScope
 import ru.inc.myalarm.extensions.room
 import ru.inc.myalarm.model.entity.room.AlarmRoom
 import ru.inc.myalarm.view_model.create.CreateAlarmRepository
 import java.util.logging.Logger
-import javax.inject.Inject
 
-@DetailsScope
-class CreateAlarmRepositoryImpl @Inject constructor(private val localDataSource: AlarmLocalDataSource) :
+
+class CreateAlarmRepositoryImpl(private val localDataSource: AlarmLocalDataSource) :
     CreateAlarmRepository {
 
     private val log = Logger.getLogger(CreateAlarmRepositoryImpl::class.java.name)
